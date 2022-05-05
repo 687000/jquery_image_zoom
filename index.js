68554256b8a3412b9591b87b1886a0cba2072021
Jiani Liu <liujianiljn@gmail.com>
2022-05-05 15:20:26 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function inTouch(e){
+    return e.type.substring(0,5)=="touch";
+}
+function ev(e){
+    if(inTouch(e)){
+        if(!e.orginalEvent.touches.length) return e.orginalEvent.changeTouches;
+        else return e.orginalEvent.touches;
+    }else {
+        return [e];
+    }
+}
+
+function getEXY(e,o){
+    let offs=$(o||e.target).offset();
+    x=e.pageX-offs.left;
+    y=e.pageY-offs.top;
+    return {x,y};
+}
+
+function getEventXY(e,o){
+    return getEXY(ev(e)[0],o);
+}
+//if a>max use max, if a<min use min. Otherwise just a.
+let clamp=(a,min,max) => a>max?max:a<min?min:a;
+$(".original-image").on("mousemove touchmove",function(e){
+    let pos=getEventXY(e,this);
+    let zoompos={
+        x:clamp(pos.x-(125*.5),0,375),
+        y:clamp(pos.y-(125*.5),0,375)
+    }
+    $(".zoomer").css({
+        transform:`translate(${zoompos.x}px,${zoompos.y}px)`
+    })
+    $(".zoomed-image").css({
+        "background-position":`${-zoompos.x*4}px ${-zoompos.y*4}px `
+    })
+})
